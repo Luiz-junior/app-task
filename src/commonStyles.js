9c7d425551022469e405a8ab1aac1a3f55256165
luiz-junior <junior.rodrigues0222@gmail.com>
2019-08-13 17:08:48 -0300

    created component Task and screen Agenda

---
+++ src/commonStyles.js
@@ -0,0 +1,9 @@
+export default {
+    fontFamily: 'Lato',
+    colors: {
+        today: '#b13d44',
+        secondary: '#fff',
+        mainText: '#222',
+        subText: '#555',
+    },
+};
---
+++ src/screens/Agenda.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import todayImage from '../../assets/imgs/today.jpg';
+import commonStyles from '../commonStyles';
+import Task from '../components/Task';
+
+class Agenda extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>
+                            {moment().locale('pt-br').format('ddd, D [de] MMM')}
+                        </Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.tasksContainer}>
+                    <Task
+                        desc="Tarefa pendente"
+                        estimateAt={new Date()}
+                        doneAt={null}
+                    />
+                    <Task
+                        desc="Tarefa concluída"
+                        estimateAt={new Date()}
+                        doneAt={new Date()}
+                    />
+                </View>
+            </View>
+        );
+    }
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 3,
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30,
+    },
+    tasksContainer: {
+        flex: 7,
+    }
+});
+
+export default Agenda;
